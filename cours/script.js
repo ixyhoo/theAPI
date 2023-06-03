a630707974a1a5b7a23416a3d08fa61c2123fae4
ixyhoo <ixyhoo@outlook.fr>
2023-06-03 14:20:13 +0200

    fetch api

--- cours/script.js
+++ cours/script.js
@@ -7,5 +7,8 @@ function reqListener() {
 let req = new XMLHttpRequest();
 req.onload = reqListener;
 
-req.open('get', 'data.txt', true);
+//req.open('get', 'data.txt', true);
+//req.open('get', 'data.json', true);
 req.send();
+
+// Fetch API
