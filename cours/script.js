4f665d5d972c3cb2fb020be9388e47732c82a6ab
ixyhoo <ixyhoo@outlook.fr>
2023-06-03 20:54:50 +0200

    fetch response

--- cours/script.js
+++ cours/script.js
@@ -9,6 +9,12 @@ req.onload = reqListener;
 
 //req.open('get', 'data.txt', true);
 //req.open('get', 'data.json', true);
-req.send();
+//req.open('get', 'https://api.blablagues.net/?rub=blagues', true);
+//req.send();
 
 // Fetch API
+
+fetch('https://api.blablagues.net/?rub=blagues').then((response) => {
+    console.log(response);
+})
+.catch((error) => console.log(error));
