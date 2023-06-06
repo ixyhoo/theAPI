2345037b84be18497edb8340000ad841a4d8d219
ixyhoo <ixyhoo@outlook.fr>
2023-06-06 10:32:18 +0200

    Js const & fetch

--- joke-app/script.js
+++ joke-app/script.js
@@ -1,1 +1,8 @@
-// https:api.blagues.net/?rub=blagues
+const header = document.getElementById('header')
+const content = document.getElementById('content')
+
+fetch('https://api.blablagues.net/?rub=blagues')
+    .then((response) => response.json())
+    .then((data) => data.data.content.text_head)
+    
+    
