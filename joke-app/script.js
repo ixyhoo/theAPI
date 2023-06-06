746a604c8e7ce765a612c86969eda533f33d6663
ixyhoo <ixyhoo@outlook.fr>
2023-06-06 10:48:56 +0200

    joke.text.content

--- joke-app/script.js
+++ joke-app/script.js
@@ -1,8 +1,20 @@
 const header = document.getElementById('header')
 const content = document.getElementById('content')
 
-fetch('https://api.blablagues.net/?rub=blagues')
-    .then((response) => response.json())
-    .then((data) => data.data.content.text_head)
+function getJoke() {
+    fetch('https://api.blablagues.net/?rub=blagues')
+        .then((response) => response.json())
+        .then((data) => {
+            const joke = data.data.content;
+            header.textContent = joke.text_head
+            content.textContent = joke.text !== ''
+            ? joke.text
+            : joke.text_hidden
+        })
+}
+
+getJoke()
+
+document.body.addEventListener('click', getJoke)    // on click, get a new joke
     
     
