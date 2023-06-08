52116f3311504166c30a7329c26850469973ced2
ixyhoo <ixyhoo@outlook.fr>
2023-06-08 10:09:33 +0200

    date parser

--- user-app/script.js
+++ user-app/script.js
@@ -2,16 +2,37 @@
 let userData = [];
 
 
-const fetchUser = () => {
+const fetchUser = async () => {
 
-    fetch("https://randomuser.me/api/?results=24")
+ await   fetch("https://randomuser.me/api/?results=24")
     .then((res)=> res.json())
     .then((data)=> userData = data.results)
     //.then(()=> console.log(userData))
 
-   /* setTimeout(() => {
+   // setTimeout(() => {
     console.log(userData);
-    }, 1000);*/
+ //   }, 1000);
 }
 
-fetchUser();
+const userDisplay = async () => {
+  await fetchUser();
+
+  const datParser = (date) => {
+    
+  }
+
+    document.body.innerHTML = userData.map(
+        (user) => 
+    `
+    <div class="card"> 
+    <img src="${user.picture.large}" alt="photo de ${user.name.last}">
+    <h3>${user.name.first}</h3>
+    <p>${user.dob.date}</p>
+    <h5>${user.location.city}, ${user.location.country}</h5>
+    <em>Membre depuis le ${user.registered.date} jours</em>
+    </div>
+    `
+    ).join("");
+};
+
+userDisplay();
