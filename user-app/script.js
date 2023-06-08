8f62a6d0e911db45399958db0db7e87d04a1f3aa
ixyhoo <ixyhoo@outlook.fr>
2023-06-08 10:45:10 +0200

    user6app is finish

--- user-app/script.js
+++ user-app/script.js
@@ -27,15 +27,29 @@ const userDisplay = async () => {
 
   }
 
+  const dayCalc = (date) => {
+        let today = new Date(date).toLocaleDateString("fr-FR", {
+            year: "numeric",
+            month: "long",
+            day: "numeric", 
+            hour: "numeric",
+        });
+        return today;
+        /*let TodayTimestamp = Date.parse(today);
+        let timestamp = Date.parse(date);
+        return Math.ceil((TodayTimestamp - timestamp) / 86400000);*/
+
+  }
+
     document.body.innerHTML = userData.map(
         (user) => 
     `
     <div class="card"> 
     <img src="${user.picture.large}" alt="photo de ${user.name.last}">
-    <h3>${user.name.first}</h3>
+    <h3>${user.name.first} ${user.name.last}</h3>
     <p>${dateParser(user.dob.date)}</p>
     <h5>${user.location.city}, ${user.location.country}</h5>
-    <em>Membre depuis le ${user.registered.date} jours</em>
+    <em>Membre depuis le ${dayCalc(user.registered.date)} </em>
     </div>
     `
     ).join("");
