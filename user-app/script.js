c6aafd590473e7887782207ca66da6242a136e9e
ixyhoo <ixyhoo@outlook.fr>
2023-06-08 10:15:32 +0200

    calc Timestamp

--- user-app/script.js
+++ user-app/script.js
@@ -17,8 +17,14 @@ const fetchUser = async () => {
 const userDisplay = async () => {
   await fetchUser();
 
-  const datParser = (date) => {
-    
+  const dateParser = (date) => {
+    let newDate = new Date(date).toLocaleDateString("fr-FR", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    });
+    return newDate;
+
   }
 
     document.body.innerHTML = userData.map(
@@ -27,7 +33,7 @@ const userDisplay = async () => {
     <div class="card"> 
     <img src="${user.picture.large}" alt="photo de ${user.name.last}">
     <h3>${user.name.first}</h3>
-    <p>${user.dob.date}</p>
+    <p>${dateParser(user.dob.date)}</p>
     <h5>${user.location.city}, ${user.location.country}</h5>
     <em>Membre depuis le ${user.registered.date} jours</em>
     </div>
