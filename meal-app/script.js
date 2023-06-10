e4d05df38ae04c0c25ec63a12974bd38e13b881e
ixyhoo <ixyhoo@outlook.fr>
2023-06-10 20:43:56 +0200

    meals display

--- meal-app/script.js
+++ meal-app/script.js
@@ -1,14 +1,26 @@
 
-const result = getElementById("result")
+const result = document.getElementById("result")
+const form = document.querySelector("form");
 let meals = [];
 
 console.log(result);
 async function fetchMeals(){
 
- await   fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=tomato").
-    then((res) =>
-    
-     res.json()).then((data) => (meals = data.meals))
+ await  fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=tomato").
+    then((res) => res.json())
+    .then((data) => (meals = data.meals));
      console.log(meals);
 }
-fetchMeals();
+
+function mealsDisplay(){
+   meals.length = 12;
+   result.innerHTML = meals.map((meal) =>
+   `li class="card">
+   <h2>${meal.strMeal}</h2>
+   `);
+}
+
+form.addEventListener("submit", (e) => {
+e.preventDefault();
+   fetchMeals().then(() => mealsDisplay());                                                           
+});
