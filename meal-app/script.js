e3aac69d711bb771d68781ca889e54a5c5f085d7
ixyhoo <ixyhoo@outlook.fr>
2023-06-10 21:09:37 +0200

    display result of search

--- meal-app/script.js
+++ meal-app/script.js
@@ -6,27 +6,38 @@ let meals = [];
 
 
 console.log(result);
-async function fetchMeals(){
+async function fetchMeals(search){
 
- await  fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=tomato").
+ await  fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + search).
     then((res) => res.json())
     .then((data) => (meals = data.meals));
      console.log(meals);
 }
 
 function mealsDisplay(){
-   meals.length = 12;
-   result.innerHTML = meals.map((meal) =>
-   `<li class="card">
-   <h2>${meal.strMeal}</h2>
-   <p>${meal.strArea}</p>
-   <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
-   <ul></ul>
-   </li>
-   `).join("");
+   if (meals === null) {
+      result.innerHTML = "<h2>There are no meals</h2>";
+   }  else {
+
+      meals.length = 12;
+      result.innerHTML = meals.map((meal) =>
+      `<li class="card">
+      <h2>${meal.strMeal}</h2>
+      <p>${meal.strArea}</p>
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+      <ul></ul>
+      </li>
+      `).join("");
+   }                                                                                                                                                                                                                                                                                                                                                                                                                     
 }
+
+input.addEventListener("input", (e) => {
+   e.preventDefault();
+   fetchMeals(e.target.value);
+   
+});                                                                                                     
 
 form.addEventListener("submit", (e) => {
 e.preventDefault();
-   fetchMeals().then(() => mealsDisplay());                                                           
-});
+   mealsDisplay();                                                           
+});                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
